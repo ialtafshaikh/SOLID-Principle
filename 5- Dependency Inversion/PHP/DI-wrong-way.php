c6b12bfff948172edf2ad2d4b25f7b7abece5d76
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-02-26 16:39:25 +0530

    dependency inversion principle - wrong way code added

---
+++ 5- Dependency Inversion/PHP/DI-wrong-way.php
@@ -0,0 +1,18 @@
+class MySQLConnection
+{
+    public function connect()
+    {
+        // handle the database connection
+        return 'Database connection';
+    }
+}
+
+class PasswordReminder
+{
+    private $dbConnection;
+
+    public function __construct(MySQLConnection $dbConnection)
+    {
+        $this->dbConnection = $dbConnection;
+    }
+}
