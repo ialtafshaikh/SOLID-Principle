e4cd933502a709c34d4ab290ab10209d4075bafd
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-02-26 16:09:16 +0530

    open closed principle - wrong way code added

---
+++ 2-Open-Closed/PHP/SRP-wrong-way.php
@@ -0,0 +1,88 @@
+class Square
+{
+    public $length;
+
+    public function construct($length)
+    {
+        $this->length = $length;
+    }
+}
+
+class Circle
+{
+    public $radius;
+
+    public function construct($radius)
+    {
+        $this->radius = $radius;
+    }
+}
+
+
+class AreaCalculator
+{
+    protected $shapes;
+
+    public function __construct($shapes = [])
+    {
+        $this->shapes = $shapes;
+    }
+
+    public function sum()
+    {
+        foreach ($this->shapes as $shape) {
+            if (is_a($shape, 'Square')) {
+                $area[] = pow($shape->length, 2);
+            } elseif (is_a($shape, 'Circle')) {
+                $area[] = pi() * pow($shape->radius, 2);
+            }
+        }
+
+        return array_sum($area);
+    }
+
+}
+
+
+class SumCalculatorOutputter
+{
+    protected $calculator;
+
+    public function __constructor(AreaCalculator $calculator)
+    {
+        $this->calculator = $calculator;
+    }
+
+    public function JSON()
+    {
+        $data = [
+          'sum' => $this->calculator->sum(),
+      ];
+
+        return json_encode($data);
+    }
+
+    public function HTML()
+    {
+        return implode('', [
+          '',
+              'Sum of the areas of provided shapes: ',
+              $this->calculator->sum(),
+          '',
+      ]);
+    }
+}
+ 
+
+
+$shapes = [
+  new Circle(2),
+  new Square(5),
+  new Square(6),
+];
+
+$areas = new AreaCalculator($shapes);
+$output = new SumCalculatorOutputter($areas);
+
+echo $output->JSON();
+echo $output->HTML();
