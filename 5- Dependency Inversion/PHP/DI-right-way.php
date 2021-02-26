0e36751608b622f1da5ff45c6f57680f60746818
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-02-26 16:40:09 +0530

    dependency inversion principle - implemented

---
+++ 5- Dependency Inversion/PHP/DI-right-way.php
@@ -0,0 +1,23 @@
+interface DBConnectionInterface
+{
+    public function connect();
+}
+
+class MySQLConnection implements DBConnectionInterface
+{
+    public function connect()
+    {
+        // handle the database connection
+        return 'Database connection';
+    }
+}
+
+class PasswordReminder
+{
+    private $dbConnection;
+
+    public function __construct(DBConnectionInterface $dbConnection)
+    {
+        $this->dbConnection = $dbConnection;
+    }
+}
