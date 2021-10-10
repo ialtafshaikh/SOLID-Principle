ddec5f2d47429ae71570341a814de334f487926c
Altaf Shaikh <iamaltafshaikh07@gmail.com>
2021-10-10 10:16:41 +0530

    SR | Python lang added

---
+++ 1- Single Responsibility (S)/Python/SR-right-way.py
@@ -0,0 +1,18 @@
+def get_mean(list_):
+    '''Compute Max'''
+    print(f"the mean is {np.mean(list_)}") 
+
+def get_max(list_):
+    '''Compute Max'''
+    print(f"the max is {np.max(list_)}") 
+
+def main(list_): 
+    # Compute Average
+    get_mean(list_)
+    # Compute Max
+    get_max(list_)
+
+main([1,2,3,4,5])
+# the mean is 3.0
+# the max is 5
+view raw
